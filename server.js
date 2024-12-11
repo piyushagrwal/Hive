2ca1af8f0235d29465792050dae910eaee533568
Piyush Agarwal <piyushagarwal@Piyushs-MacBook-Air.local>
2024-12-11 10:25:43 +0530

    Updating callback URL

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ passport.deserializeUser((obj, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:5173/api/v1/auth/google/callback',
+    callbackURL: 'https://hive-upjh.onrender.com/api/v1/auth/google/callback',
     scope: ["profile", "email"]
 },
 async(accessToken, refreshToken, profile, done) => {
